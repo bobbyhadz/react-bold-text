137e2888b363616694bfe3e960b3aff76804dbec
bobbyhadz <opmftw@gmail.com>
2024-04-06 14:32:18 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <p style={{fontSize: '2rem'}}>
+        Hello <span style={{fontWeight: 'bold'}}>world</span>
+        !!!
+      </p>
+    </div>
+  );
+};
+
+export default App;
